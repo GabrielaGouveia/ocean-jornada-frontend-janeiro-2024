0e0133f5aeef5ff5aa45f273f408f34425b7eae7
Gabriela Gouveia <111470667+GabrielaGouveia@users.noreply.github.com>
2024-01-30 12:50:00 +0000

    Criação e centralização das tags

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,18 @@
+import React from 'react';
+
 function Card() {
   return (
-    <>
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+    <div className="card">
+      <h2>Rick Sanchez</h2>
+      <div className='tags'>
+      <div className='tag'>Status: Vivo</div>
+      <div className='tag'>Espécie: Humana</div>
+      <div className='tag'>Origem: Terra C-137</div>
       </div>
-    </>
-  )
+      <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Rick Sanchez" />
+    </div>
+  );
 }
 
-export default Card
+export default Card;
+
